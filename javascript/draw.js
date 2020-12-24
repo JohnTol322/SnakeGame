829058d188b96504a609a428c23ae69f5530f793
John Tol <70571076+JohnTol322@users.noreply.github.com>
2020-12-24 01:01:13 +0100

    🐍🎉 GAME FINISHED

---
+++ javascript/draw.js
@@ -0,0 +1,39 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const upimg = document.getElementById('up-img');
+const downimg = document.getElementById('down-img');
+const rightimg = document.getElementById('right-img');
+const leftimg = document.getElementById('left-img');
+const fruitimg = document.getElementById('fruit-img');
+const tailimg = document.getElementById('tail-img');
+const scale = (canvas.width + canvas.height) / 40;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+(function setup(){
+    snake = new Snake();
+    fruit = new Fruit();
+
+    fruit.pickLocation();
+
+    window.setInterval(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        fruit.draw();
+        snake.update();
+        snake.draw();
+
+        if(snake.eat(fruit)){
+            fruit.pickLocation();
+            fruit.draw();
+        }
+
+        snake.checkCollision();
+        document.querySelector(".score").innerHTML = "score: " + snake.total;
+    }, 150);
+}());
+
+window.addEventListener('keydown', function(evt){
+    const direction = evt.key.replace("Arrow", "");
+    snake.changeDirection(direction);
+});
+
---
+++ javascript/snake.js
@@ -0,0 +1,191 @@
+function Snake(){
+    this.x = scale;
+    this.y = scale;
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+    this.total = 0;
+    this.tail = [];
+    left = false;
+    right = true;
+    up = false;
+    down = false
+
+    this.draw = function() {
+        for(let i = 0; i < this.tail.length; i++){
+            ctx.drawImage(tailimg, this.tail[i].x, this.tail[i].y, scale, scale);
+        }
+        if(right){
+            ctx.drawImage(rightimg, this.x, this.y, scale, scale);
+        }else if(left){
+            ctx.drawImage(leftimg, this.x, this.y, scale, scale);
+        }else if(down){
+            ctx.drawImage(downimg, this.x, this.y, scale, scale);
+        }else if(up){
+            ctx.drawImage(upimg, this.x, this.y, scale, scale);
+        }
+    }
+
+    this.update = function() {
+
+        for(let i = 0; i < this.tail.length -1; i++){
+            this.tail[i] = this.tail[i+1];
+        }
+        
+        this.tail[this.total -1] = { x: this.x, y: this.y };
+
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+
+        if(this.x >= canvas.width){
+            this.die();
+        }
+        else if(this.y >= canvas.height){   
+            this.die()
+        }
+        else if(this.x < 0){   
+            this.die();
+        }
+        else if(this.y < 0){   
+            this.die();
+        }
+    }
+
+    this.changeDirection = function(direction) {
+        switch(direction){
+            case "Up":
+                if(this.ySpeed != 0){
+                    break;
+                }
+                else{
+                this.xSpeed = 0;
+                this.ySpeed = scale * -1;
+                up = true;
+                down = false;
+                left = false;
+                right = false;
+                break; 
+                }    
+            case "Down":
+                if(this.ySpeed != 0){
+                    break;
+                }
+                else{
+                this.xSpeed = 0;
+                this.ySpeed = scale * 1;
+                up = false;
+                down = true;
+                left = false;
+                right = false;
+                break; 
+                }    
+            case "Left":
+                if(this.xSpeed != 0){
+                    break;
+                }
+                else{
+                this.xSpeed = scale * -1;
+                this.ySpeed = 0;
+                up = false;
+                down = false;
+                left = true;
+                right = false;
+                break; 
+                }    
+            case "Right":
+                if(this.xSpeed != 0){
+                    break;
+                }
+                else{
+                this.xSpeed = scale * 1;
+                this.ySpeed = 0;
+                up = false;
+                down = false;
+                left = false;
+                right = true;
+                break; 
+                }
+            case "w":
+                if(this.ySpeed != 0){
+                    break;
+                }
+                else{
+                this.xSpeed = 0;
+                this.ySpeed = scale * -1;
+                up = true;
+                down = false;
+                left = false;
+                right = false;
+                break; 
+                }    
+            case "s":
+                if(this.ySpeed != 0){
+                    break;
+                }
+                else{
+                this.xSpeed = 0;
+                this.ySpeed = scale * 1;
+                up = false;
+                down = true;
+                left = false;
+                right = false;
+                break; 
+                }    
+            case "a":
+                if(this.xSpeed != 0){
+                    break;
+                }
+                else{
+                this.xSpeed = scale * -1;
+                this.ySpeed = 0;
+                up = false;
+                down = false;
+                left = true;
+                right = false;
+                break; 
+                }    
+            case "d":
+                if(this.xSpeed != 0){
+                    break;
+                }
+                else{
+                this.xSpeed = scale * 1;
+                this.ySpeed = 0;
+                up = false;
+                down = false;
+                left = false;
+                right = true;
+                break; 
+                }     
+        }
+    }
+
+    this.eat = function(){
+        if(this.x == fruit.x && this.y == fruit.y){
+            this.total++
+            return true;
+        }
+        return false;
+    }
+
+    this.checkCollision = function(){
+        for(let i = 0; i < this.tail.length; i++){
+            if(this.x == this.tail[i].x && this.y == this.tail[i].y){
+                this.die();
+            }
+            
+        }
+    }
+
+    this.die = function(){
+        this.total = 0;
+        this.tail = [];
+        this.x = scale;
+        this.y = scale;
+        this.xSpeed = 0;
+        this.ySpeed = 0;
+        up = false;
+        down = false;
+        left = false;
+        right = true;
+    }
+}
